c88ed3868070a8bef29452564ef4ea041ff5ff01
Alex <alexjooho@gmail.com>
2022-09-13 12:15:51 -0700

    trying to figure out how aws works

---
+++ express-sharebnb/routes/properties.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** Routes for properties. */
+
+const express = require("express");
+const router = new express.Router();
+
+/** POST /properties */
+
+router.post("/properties", function (req, res, next) {
+    
+}
---
+++ express-sharebnb/s3_buckets.js
@@ -0,0 +1,13 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-west-1'});
+
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+s3.listBuckets(function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.Buckets);
+    }
+  });
